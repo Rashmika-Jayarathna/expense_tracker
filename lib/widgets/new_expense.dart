a1ccf87efec392c7b50ace8aedb108172921b367
Rashmika-Jayarathna <rashmikajayarathna@gmail.com>
2024-09-13 12:51:57 +0530

    Added input fields for add new expense

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,130 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseStaet();
+  }
+}
+
+class _NewExpenseStaet extends State<NewExpense> {
+  final _titleConatroller = TextEditingController();
+  final _priceConatroller = TextEditingController();
+  DateTime? _selectedDate;
+  Category _seectedCategory = Category.food;
+
+  @override
+  void dispose() {
+    _titleConatroller.dispose();
+    _priceConatroller.dispose();
+    super.dispose();
+  }
+
+  void presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            controller: _titleConatroller,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _priceConatroller,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? 'No date Selected'
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _seectedCategory,
+                  items: Category.values
+                      .map(
+                        (item) => DropdownMenuItem(
+                          value: item,
+                          child: Text(
+                            item.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _seectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleConatroller.text);
+                  print(_priceConatroller.text);
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
